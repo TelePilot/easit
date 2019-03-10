2a9f08a2f7d9265a54fe05ef627f01f8a61b056f
Linus Bein Fahlander <contact@linusbf.com>
2019-03-10 13:41:11 +0100

    Some changes to the way news posts are displayed

--- web/app/themes/understrap-child/loop-templates/content-single.php
+++ web/app/themes/understrap-child/loop-templates/content-single.php
@@ -6,11 +6,6 @@
  */
 
 ?>
-<?php
-$startDate = am_get_the_startdate('U');
-$endDate = am_get_the_enddate('U');
-$startTime = am_get_the_startdate('H:i');
-?>
 
 <article <?php post_class(); ?> id="post-
     <?php the_ID(); ?>">
@@ -18,39 +13,13 @@ $startTime = am_get_the_startdate('H:i');
         <div class="row">
             <div class="col-md-6">
                 <h2 class="card-title"><?php the_title(); ?></h2>
-      <?php if ( $startTime !== '00:00') { ?>
-      <h4 class="card-subtitle text-capitalize">
-          <?php am_the_startdate('l', '<b>', ' </b>');
 
-          am_the_startdate('j F H:i');
-          if ($startDate !== $endDate) {
-            am_the_enddate('H:i', ' - ');
-          }
-        ?>
-      </h4>
-    <?php } ?>
-
-      <p class="card-text pt-3"><?php echo wp_trim_words(get_the_content(), 1000); ?></p>
+                <p class="card-text pt-3"><?php echo wp_trim_words(get_the_content(), 1000); ?></p>
 
             </div>
             <div class="col-md-6">
-                <?php echo get_the_post_thumbnail( $post->ID, 'full' ); ?>
+                <?php echo get_the_post_thumbnail($post->ID, 'full'); ?>
             </div>
         </div>
     </div>
-
-
-
-
-
-
-
-
-
-    <footer class="entry-footer">
-
-        <?php understrap_entry_footer(); ?>
-
-    </footer><!-- .entry-footer -->
-
 </article><!-- #post-## -->
--- web/app/themes/understrap-child/single.php
+++ web/app/themes/understrap-child/single.php
@@ -10,37 +10,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 get_header();
-$container   = get_theme_mod( 'understrap_container_type' );
 ?>
-<?php get_template_part( 'loop-templates/subtop', 'page' ); ?>
-<div class="wrapper" id="single-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+<div id="single-post" class="container">
+    <?php get_template_part( 'loop-templates/content-visual', 'page' ); ?>
+</div>
 
-		<div class="row">
-			<main class="site-main" id="main">
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'loop-templates/content', 'single' ); ?>
-
-						<?php understrap_post_nav(); ?>
-
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
-
-				<?php endwhile; // end of the loop. ?>
-
-			</main><!-- #main -->
-
-	</div><!-- .row -->
-
-</div><!-- Container end -->
-
-</div><!-- Wrapper end -->
+<!-- Wrapper end -->
 
 <?php get_footer(); ?>
