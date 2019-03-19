6108dc761aa399e0eacd31aa4bc64c850e23d101
Linus Bein Fahlander <contact@linusbf.com>
2019-03-19 21:38:54 +0100

    Initial search result type solution

--- web/app/themes/understrap-child/loop-templates/content-search.php
+++ web/app/themes/understrap-child/loop-templates/content-search.php
@@ -10,7 +10,17 @@
 
     <header class="search-header">
 
-        <h6 class="search-result-type"><?php echo (get_post_type() === "page" ? __("Page", "understrap") : get_the_category()); ?></h6>
+        <?php
+            $post_type = "";
+            if(get_post_type() === "page"){
+                $post_type = __("Page", "understrap");
+            } else {
+                (count($cats = get_the_category()) > 0 ? $post_type = __($cats[0]->name, "understrap") : __("Page", "understrap"));
+                //TODO - Decide on fallback
+            }
+        ?>
+
+        <h6 class="search-result-type"><?php echo $post_type; ?></h6>
         <?php the_title( sprintf( '<h4 class="search-result-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
             '</a></h4>' ); ?>
         <h6 class="search-result-link"><?php echo esc_url(get_permalink()); ?></h6>
