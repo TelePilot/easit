de50630df289cecde5fc0ea3d19e6d2de7cdfc49
Linus Bein Fahlander <contact@linusbf.com>
2019-03-19 20:29:04 +0100

    Basic search result page finished

--- web/app/themes/understrap-child/loop-templates/content-search.php
+++ web/app/themes/understrap-child/loop-templates/content-search.php
@@ -6,18 +6,18 @@
  */
 
 ?>
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class("search-result"); ?> id="post-<?php the_ID(); ?>">
 
-    <header class="entry-header">
+    <header class="search-header">
 
-        <?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-            '</a></h3>' ); ?>
+        <h6 class="search-result-type"><?php echo (get_post_type() === "page" ? __("Page", "understrap") : get_the_category()); ?></h6>
+        <?php the_title( sprintf( '<h4 class="search-result-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+            '</a></h4>' ); ?>
+        <h6 class="search-result-link"><?php echo esc_url(get_permalink()); ?></h6>
 
     </header><!-- .entry-header -->
 
-
-
-    <div class="entry-summary">
+    <div class="search-summary">
 
         <?php the_excerpt(); ?>
 
