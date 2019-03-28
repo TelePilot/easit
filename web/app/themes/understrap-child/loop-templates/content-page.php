147c30a3a6c6829d7c13813af88939d8ebe1c875
Linus Bein Fahlander <contact@linusbf.com>
2019-03-28 20:46:00 +0100

    Added child theme translations to POT file in base understrap theme

--- web/app/themes/understrap-child/loop-templates/content-page.php
+++ web/app/themes/understrap-child/loop-templates/content-page.php
@@ -24,7 +24,7 @@
 
 		<?php
 		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap-child' ),
 			'after'  => '</div>',
 		) );
 		?>
@@ -34,7 +34,7 @@
 
 	<footer class="entry-footer">
 
-		<?php edit_post_link( __( 'Edit', 'understrap' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', 'understrap-child' ), '<span class="edit-link">', '</span>' ); ?>
 
 	</footer><!-- .entry-footer -->
 
